7c3c81a9bc1e0a5896774d2d851ebbcc4dbf9063
John Creecy <johncreecy@gmail.com>
2016-12-19 12:42:25 -0500

    Config tweak

--- input/InputMomentum.cpp
+++ input/InputMomentum.cpp
@@ -5,7 +5,7 @@ InputMomentum::InputMomentum()
 {
     mDecayRate.x = 0.99;
     mDecayRate.y = 0.99;
-    mDecayCounterLimit = 300;
+    mDecayCounterLimit = 30;
     SDL_Log("InputMomentum::InputMomentum -- mDecayCounterLimit: %d DecayRate: [x: %f y: %f] \n", mDecayCounterLimit, mDecayRate.x, mDecayRate.y);
 
     mDecayCounter = 0;
@@ -21,7 +21,7 @@ void InputMomentum::update(const int & x, const int & y)
     mMomentum.x = x;
     mMomentum.y = y;
     mDecayCounter = 0;
-    SDL_Log("InputMomentum::updateMomentum -- mMomentum: [x: %d y: %d] \n", mMomentum.x, mMomentum.y);
+    // SDL_Log("InputMomentum::updateMomentum -- mMomentum: [x: %d y: %d] \n", mMomentum.x, mMomentum.y);
 }
 
 const PointInt & InputMomentum::decay()
@@ -37,6 +37,6 @@ const PointInt & InputMomentum::decay()
         mMomentum.x = 0;
         mMomentum.y = 0;
     }
-    SDL_Log("InputMomentum::decayMomentum -- mDecayCounter: %d mMomentum: [x: %d y: %d] \n", mDecayCounter, mMomentum.x, mMomentum.y);
+    // SDL_Log("InputMomentum::decayMomentum -- mDecayCounter: %d mMomentum: [x: %d y: %d] \n", mDecayCounter, mMomentum.x, mMomentum.y);
     return mMomentum;
 }
