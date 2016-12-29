46c2ed7f31736c5dc0571186ff3c4a6b36cef2ee
John Creecy <johncreecy@gmail.com>
2016-12-29 17:03:57 -0500

    StatusMenu progress - display players remaining credits

--- renderers/StatusMenuRenderer.cpp
+++ renderers/StatusMenuRenderer.cpp
@@ -3,6 +3,7 @@
 StatusMenuRenderer::StatusMenuRenderer()
 {
     mSDLRenderer = WindowManager::getInstance()->getSDLRenderer();
+    mLabelTextProfile = "Heading_3_OpenSansLight";
 }
 
 StatusMenuRenderer::~StatusMenuRenderer()
@@ -24,6 +25,7 @@ void StatusMenuRenderer::attach(const Viewport &viewport)
 
     SDL_Log("----------------------------------------------------\n");
     SDL_Log("StatusMenuRenderer::attached -- setting up ... \n");
+    setLayout();
     SDL_Log("----------------------------------------------------\n");
 }
 
@@ -33,26 +35,98 @@ void StatusMenuRenderer::detatch()
     SDL_Log("StatusMenuRenderer::detatch -- detatched, rendering stopped. \n");
 }
 
+
 void StatusMenuRenderer::render()
 {
     if (mAttached)
     {
-        renderBackground();
-        renderBorder();
+        drawBorders();
+        renderText("Stats", mHeading);
+        renderCreditsRemaining(mCreditsRemain);
     }
 }
 
-// TODO configs
-void StatusMenuRenderer::renderBorder()
+void StatusMenuRenderer::setLayout()
 {
-    SDL_SetRenderDrawColor(mSDLRenderer, 0, 255, 0, 255);
-    SDL_RenderDrawRect(mSDLRenderer, &mBorder);
+    mHeading = getCellRect(0, 0, 0.20);
+    mCreditsRemain = getCellRect(0, mHeading.y + mHeading.h, 0.20);
 }
 
-void StatusMenuRenderer::renderBackground()
+void StatusMenuRenderer::drawBorders()
+{
+    renderBackground(mHeading);
+    renderBorder(mHeading);
+    renderBackground(mCreditsRemain);
+    renderBorder(mCreditsRemain);
+}
+
+void StatusMenuRenderer::renderCreditsRemaining(const SDL_Rect & cellRect)
+{
+    const std::string credits = "Credits: " + intToStr(Player::Instance().getCredits());
+
+    renderText(credits, cellRect);
+}
+
+SDL_Rect StatusMenuRenderer::getCellRect(const int & offsetX, const int & offsetY, const double & scale)
+{
+    const int xPadding = 4;
+    const int yPadding = 4;
+
+    SDL_Rect rect;
+
+    rect.x = (mBorder.x + xPadding) + offsetX;
+    rect.y = (mBorder.y + yPadding) + offsetY;
+    rect.w = mBorder.w - (xPadding * 2);
+    rect.h = (mBorder.h * scale) - (yPadding * 2);
+
+    SDL_Log("SelectedActionMenuRenderer::getCellRect -- rect: [x: %d y: %d w: %d h: %d] \n",
+            rect.x,
+            rect.y,
+            rect.w,
+            rect.h);
+
+    return rect;
+}
+
+std::string StatusMenuRenderer::intToStr(const int & value)
+{
+    std::string result;
+    std::ostringstream convert;
+    convert << value;
+    result = convert.str();
+    return result;
+}
+
+void StatusMenuRenderer::renderBorder(const SDL_Rect & cellRect)
 {
     SDL_SetRenderDrawColor(mSDLRenderer, 0, 255, 0, 255);
-    SDL_RenderFillRect(mSDLRenderer, &mBorder);
+    SDL_RenderDrawRect(mSDLRenderer, &cellRect);
+}
+
+void StatusMenuRenderer::renderBackground(const SDL_Rect & cellRect)
+{
+    SDL_SetRenderDrawColor(mSDLRenderer, 100, 0, 150, 255);
+    SDL_RenderFillRect(mSDLRenderer, &cellRect);
+}
+
+void StatusMenuRenderer::renderText(const std::string & text, const SDL_Rect & cellRect)
+{
+
+    FontTextures & fontTextures = FontManager::getInstance()->getTextures(mLabelTextProfile, text);
+
+    SDL_Texture *labelTexture = fontTextures.getTexture(text);
+    const SDL_Rect & textureSize = fontTextures.getRect(text);
+
+    SDL_Rect renderRect = { cellRect.x, cellRect.y, textureSize.w, textureSize.h };
+    const int heightPadding = (cellRect.h - textureSize.h) / 2;
+
+    renderRect.y = cellRect.y + heightPadding;         // center y axis
+
+    // default left alignment
+    const int textPadding = 5;
+    renderRect.x = cellRect.x + textPadding;
+
+    SDL_RenderCopy(mSDLRenderer, labelTexture, NULL, &renderRect);
 }
 
 void StatusMenuRenderer::renderSprite(const SpriteProperties & spriteProperties, const SDL_Rect & destRect)
